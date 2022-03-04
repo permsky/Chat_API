ce147c1f1627556a05131bcde8e340cc7da034ba
Vitaly Borodulin <rainbowf@mail.ru>
2022-03-04 23:16:24 +0500

    Override post method for creating User, Chat and Message objects

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
 
-from .models import Chat, Message
+from .models import Chat, Message, User
 from .serializers import (
     UserSerializer,
     ChatSerializer,
@@ -12,13 +12,32 @@ from .serializers import (
 class CreateUserAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
+    def post(self,request):
+        user = User.objects.create(username=self.request.data.get('username'))
+        return Response({'id': user.id})
+
 
 class CreateChatAPIView(generics.CreateAPIView):
     serializer_class = ChatSerializer
 
+    def post(self,request):
+        chat = Chat.objects.create(
+            name=self.request.data.get('name'),
+        )
+        chat.users.set(self.request.data.get('users'))
+        return Response({'id': chat.id})
+
 
 class CreateMessageAPIView(generics.CreateAPIView):
     serializer_class = MessageSerializer
+
+    def post(self,request):
+        message = Message.objects.create(
+            chat=Chat.objects.get(id=self.request.data.get('chat')),
+            author=User.objects.get(id=self.request.data.get('author')),
+            text=self.request.data.get('text')
+        )
+        return Response({'id': message.id})
 
 
 class GetChatsAPIView(generics.ListCreateAPIView):
@@ -31,7 +50,7 @@ class GetChatsAPIView(generics.ListCreateAPIView):
     def post(self,request):
         chats = self.get_queryset()
         serializer = self.serializer_class(chats, many=True)
-        return Response({'chats': serializer.data})
+        return Response(serializer.data)
 
 
 class GetMessagesAPIView(generics.ListAPIView):
@@ -44,4 +63,4 @@ class GetMessagesAPIView(generics.ListAPIView):
     def post(self,request):
         messages = self.get_queryset()
         serializer = self.serializer_class(messages, many=True)
-        return Response({'messages': serializer.data})
+        return Response(serializer.data)
